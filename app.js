c8eb422836ea38f04178fb2ad3777dfcbc2a5be7
aluay <49422693+aluay@users.noreply.github.com>
2021-11-28 14:07:26 -0800

    Bug fixes for mobile views

--- app.js
+++ app.js
@@ -7,49 +7,49 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/"));
 app.set("view engine", "ejs");
 app.use(
-    bp.urlencoded({
-        extended: false,
-    })
+  bp.urlencoded({
+    extended: false,
+  })
 );
 app.use(bp.json());
 
 //  Render the index page
-app.get("/", async(req, res) => {
-    res.render("index");
+app.get("/", async (req, res) => {
+  res.render("index");
 });
 
 //  One function to generate the QR codes
-app.post("/generate", async(req, res) => {
-    //  QR options
-    //  By default the colors are black for the pattern and white for the background
-    const qrBGColor = req.body.qrBGColor;
-    const qrPatternColor = req.body.qrPatternColor;
-    const qrOptions = {
-        errorCorrectionLevel: "H",
-        margin: 0.5,
-        width: 1000,
-        height: 1000,
-        color: {
-            dark: qrPatternColor,
-            light: qrBGColor,
-        },
-    };
-    //  encode the data to a base64 string
-    QRCode.toDataURL(req.body.qrData, qrOptions, (err, src) => {
-        if (err) res.send("Something went wrong!");
-        const img = Buffer.from(src);
-        //  Send the image back to the frontend
-        res.send(img);
-    });
+app.post("/generate", async (req, res) => {
+  //  QR options
+  //  By default the colors are black for the pattern and white for the background
+  const qrBGColor = req.body.qrBGColor;
+  const qrPatternColor = req.body.qrPatternColor;
+  const qrOptions = {
+    errorCorrectionLevel: "H",
+    margin: 0.5,
+    width: 1000,
+    height: 1000,
+    color: {
+      dark: qrPatternColor,
+      light: qrBGColor,
+    },
+  };
+  //  encode the data to a base64 string
+  QRCode.toDataURL(req.body.qrData, qrOptions, (err, src) => {
+    if (err) res.send("Something went wrong!");
+    const img = Buffer.from(src);
+    //  Send the image back to the frontend
+    res.send(img);
+  });
 });
 
 //  404 redirects to home page
 //  I don't think a separate 404 page is required at the moment
 //  Maybe in the future?
-app.get('*', function(req, res) {
-    res.render("index");
+app.get("*", function (req, res) {
+  res.render("index");
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+  console.log(`Running on port ${PORT}`);
 });
--- js/simpleqrgen.js
+++ js/simpleqrgen.js
@@ -1,16 +1,16 @@
 //  Tabs handler
 // Switches between tabs and changes the color of the activated tab
 function openTab(tabName, activeTab) {
-    let tab = document.getElementsByClassName("tab");
-    let button = document.getElementsByClassName('tButton');
-    let selectedTab = document.getElementById(tabName);
-    let active = document.getElementById(activeTab);
-    for (let i = 0; i < tab.length; i++) {
-        tab[i].style.display = "none";
-        button[i].style.backgroundColor = "var(--darkBGColor)";
-    }
-    active.style.backgroundColor = "var(--surfaceColor)";
-    selectedTab.style.display = "block";
+  let tab = document.getElementsByClassName("tab");
+  let button = document.getElementsByClassName("tButton");
+  let selectedTab = document.getElementById(tabName);
+  let active = document.getElementById(activeTab);
+  for (let i = 0; i < tab.length; i++) {
+    tab[i].style.display = "none";
+    button[i].style.backgroundColor = "var(--darkBGColor)";
+  }
+  active.style.backgroundColor = "var(--surfaceColor)";
+  selectedTab.style.display = "block";
 }
 
 //  Character counter
@@ -21,123 +21,139 @@ const length = textarea.getAttribute("maxlength");
 const count = document.getElementById("counterDiv");
 const counterDiv = document.getElementById("counterDiv");
 count.innerHTML = length;
-textarea.onkeyup = function() {
-    count.innerHTML = length - this.value.length;
-    if (this.value.length >= 1000) {
-        count.classList.remove("countLessThan");
-        count.classList.add("countMoreThan");
-    }
-    if (this.value.length < 1000) {
-        count.classList.remove("countMoreThan");
-        count.classList.add("countLessThan");
-    }
+textarea.onkeyup = function () {
+  count.innerHTML = length - this.value.length;
+  if (this.value.length >= 1000) {
+    count.classList.remove("countLessThan");
+    count.classList.add("countMoreThan");
+  }
+  if (this.value.length < 1000) {
+    count.classList.remove("countMoreThan");
+    count.classList.add("countLessThan");
+  }
 };
 
 //  Text qr generator
 //  Format the input and sends it to the server
-$("#textQR").keyup(function(event) {
-    event.preventDefault();
-    const qrData = $("#textInput").val();
-    //  Stop form submission if qrData is empty
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#textQR").keyup(function (event) {
+  event.preventDefault();
+  const qrData = $("#textInput").val();
+  //  Stop form submission if qrData is empty
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  URL qr generator
 //  Format the input and sends it to the server
-$("#urlQR").keyup(function(event) {
-    event.preventDefault();
-    const qrData = $("#urlInput").val();
-    //  Stop form submission if qrData is empty
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#urlQR").keyup(function (event) {
+  event.preventDefault();
+  const qrData = $("#urlInput").val();
+  //  Stop form submission if qrData is empty
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  WiFi qr generator
 //  Format the input and sends it to the server
-$("#wifiQR").on("keyup change", function() {
-    const qrData = `WIFI:S:${$("#wifiInput").val()};T:${$("#wifiEncryption").val()};P:${$("#wifiPasswordInput").val()};;`;
-    //  Stop form submission if qrData is empty
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#wifiQR").on("keyup change", function () {
+  const qrData = `WIFI:S:${$("#wifiInput").val()};T:${$(
+    "#wifiEncryption"
+  ).val()};P:${$("#wifiPasswordInput").val()};;`;
+  //  Stop form submission if qrData is empty
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  SMS qr generator
 //  Format the input and sends it to the server
-$("#smsQR").keyup(function() {
-    const qrData = `SMSTO:${$("#phoneNumber").val()}:${$("#messageToSend").val()}`;
-    //  Stop form submission if qrData is empty
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#smsQR").keyup(function () {
+  const qrData = `SMSTO:${$("#phoneNumber").val()}:${$(
+    "#messageToSend"
+  ).val()}`;
+  //  Stop form submission if qrData is empty
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  Email qr generator
 //  Format the input and sends it to the server
-$("#emailQR").keyup(function() {
-    const qrData = `mailto:${$("#emailAddress").val()}?subject=${$("#subject").val()}&body=${$("#emailToSend").val()}`
-        //  Stop form submission if qrData is empty
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#emailQR").keyup(function () {
+  const qrData = `mailto:${$("#emailAddress").val()}?subject=${$(
+    "#subject"
+  ).val()}&body=${$("#emailToSend").val()}`;
+  //  Stop form submission if qrData is empty
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  Phone qr generator
 //  Format the input and sends it to the server
-$("#phoneQR").keyup(function(event) {
-    event.preventDefault();
-    const qrData = `tel:${$("#phone").val()}`;
-    //  Stop form submission if qrData is empty
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#phoneQR").keyup(function (event) {
+  event.preventDefault();
+  const qrData = `tel:${$("#phone").val()}`;
+  //  Stop form submission if qrData is empty
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  vCard qr generator
 //  Format the input and sends it to the server
-$("#vcardQR").keyup(function(event) {
-    event.preventDefault();
-    //  VERY long const since the vCard format has lots of details
-    const qrData = `BEGIN:VCARD\nVERSION:3\nN:${$("#lastName").val()};${$("#firstName").val()}\nORG:${$("#org").val()}\nTITLE:${$("#workTitle").val()}\nURL:${$("#website").val()}\nEMAIL;TYPE=INTERNET:${$("#email").val()}\nTEL;CELL:${$("#personalPhoneNumber").val()}\nTEL;WORK;VOICE:${$("#workPhoneNumber").val()}\nADR:;;${$("#streetAddress").val()};${$("#city").val()};${$("#state").val()};${$("#zipcode").val()};${$("#country").val()}\nEND:VCARD`;
-    if (qrData) {
-        sendToGenerator(qrData);
-    } else {
-        return false;
-    }
+$("#vcardQR").keyup(function (event) {
+  event.preventDefault();
+  //  VERY long const since the vCard format has lots of details
+  const qrData = `BEGIN:VCARD\nVERSION:3\nN:${$("#lastName").val()};${$(
+    "#firstName"
+  ).val()}\nORG:${$("#org").val()}\nTITLE:${$("#workTitle").val()}\nURL:${$(
+    "#website"
+  ).val()}\nEMAIL;TYPE=INTERNET:${$("#email").val()}\nTEL;CELL:${$(
+    "#personalPhoneNumber"
+  ).val()}\nTEL;WORK;VOICE:${$("#workPhoneNumber").val()}\nADR:;;${$(
+    "#streetAddress"
+  ).val()};${$("#city").val()};${$("#state").val()};${$("#zipcode").val()};${$(
+    "#country"
+  ).val()}\nEND:VCARD`;
+  if (qrData) {
+    sendToGenerator(qrData);
+  } else {
+    return false;
+  }
 });
 
 //  One method that gets called by jQuery code above
 //  It takes the data and sends it to the generator
-const sendToGenerator = async(qrData) => {
-    $.ajax({
-        global: false,
-        type: "POST",
-        url: "/generate",
-        dataType: "html",
-        data: {
-            qrData
-        },
-        success: function(result) {
-            //  update the img element SRC to show newly generated QR code
-            $("#qrImg").attr("src", result);
-        },
-        error: function(request, status, error) {
-            console.log(error);
-        },
-    });
-}
+const sendToGenerator = async (qrData) => {
+  $.ajax({
+    global: false,
+    type: "POST",
+    url: "/generate",
+    dataType: "html",
+    data: {
+      qrData,
+    },
+    success: function (result) {
+      //  update the img element SRC to show newly generated QR code
+      $("#qrImg").attr("src", result);
+    },
+    error: function (request, status, error) {
+      console.log(error);
+    },
+  });
+};
